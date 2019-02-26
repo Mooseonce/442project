3a3b96df83df0497365d8ff1e937fc5117babc64
Steven <>
2019-02-26 15:14:48 -0500

    buttons to spawn toy box items, buttons interact to physical touch

---
+++ 442Unity/Assets/_scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float timer;
+    public GameObject itemToSpawn;
+    public Transform spawnLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0) { timer -= Time.deltaTime; }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && timer <= 0)
+        {
+            Instantiate(itemToSpawn,spawnLocation.position + transform.up,spawnLocation.rotation);
+            timer = 1.0f; // to prevent the player from accidedntly double tapping.
+        }
+    }
+}
