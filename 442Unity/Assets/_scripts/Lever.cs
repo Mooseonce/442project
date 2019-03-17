1846d4952e76864d81f81259ae067c4535292d7f
unknown <dvdonato@gmail.com>
2019-03-17 15:24:15 -0400

    rebalance lever to slide open/close smoother

--- 442Unity/Assets/_scripts/Lever.cs
+++ 442Unity/Assets/_scripts/Lever.cs
@@ -20,17 +20,17 @@ public class Lever : MonoBehaviour
     }
     public void HandleReleased()
     {
-        if (grabberHandle.transform.localPosition.z < 0.3f) {
+        if (grabberHandle.transform.localPosition.z < 0.4f) {
             on = false;
             ofOffLightObject.GetComponent<Renderer>().material = offColor;
-            grabberHandle.GetComponent<ConfigurableJoint>().targetPosition = new Vector3(0, 0, 0.1f);
+            grabberHandle.GetComponent<ConfigurableJoint>().targetPosition = new Vector3(0, 0, -0.2f);
         }
-        if (grabberHandle.transform.localPosition.z > 0.6f)
+        if (grabberHandle.transform.localPosition.z > 0.5f)
         {
             
             on = true;
             ofOffLightObject.GetComponent<Renderer>().material = onColor;
-            grabberHandle.GetComponent<ConfigurableJoint>().targetPosition = new Vector3(0,0,0.7f);
+            grabberHandle.GetComponent<ConfigurableJoint>().targetPosition = new Vector3(0,0,-0.7f);
 
         }
     }
