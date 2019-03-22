211cc5f6a485eaadb79f2a6f4a0a2786160f79ce
unknown <dvdonato@gmail.com>
2019-03-22 16:16:07 -0400

    construction laser uses an in game dial to selet from it's list of possible wall objects for real time room construction. The spawned wall objects are individually paintable

--- 442Unity/Assets/_scripts/Lever.cs
+++ 442Unity/Assets/_scripts/Lever.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
-    public bool on,debugbool;
+    public bool on, debugbool;
     public Material onColor, offColor;
-    public GameObject ofOffLightObject,grabberHandle,visualHandle; //has grab script, visual handle is jointed to this
-
+    public GameObject ofOffLightObject, grabberHandle, visualHandle,selectedLocObj; //has grab script, visual handle is jointed to this
+    public List<GameObject> selectorNotches;
+    public int dialSetting;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,18 +22,31 @@ public class Lever : MonoBehaviour
     }
     public void HandleReleased()
     {
-        if (grabberHandle.transform.localPosition.z < 0.4f) {
+        if (grabberHandle.transform.localPosition.z < 0.4f)
+        {
             on = false;
             ofOffLightObject.GetComponent<Renderer>().material = offColor;
             grabberHandle.GetComponent<ConfigurableJoint>().targetPosition = new Vector3(0, 0, -0.2f);
         }
         if (grabberHandle.transform.localPosition.z > 0.5f)
         {
-            
+
             on = true;
             ofOffLightObject.GetComponent<Renderer>().material = onColor;
-            grabberHandle.GetComponent<ConfigurableJoint>().targetPosition = new Vector3(0,0,-0.7f);
+            grabberHandle.GetComponent<ConfigurableJoint>().targetPosition = new Vector3(0, 0, -0.7f);
 
         }
+    }
+    public void DialReleased()
+    {
+        int tempint = 0;
+        int selectedInt = 0;
+        float tempfloat = 1.0f;
+        foreach (GameObject go in selectorNotches)
+        { if (Vector3.Distance(go.transform.position, selectedLocObj.transform.position) < tempfloat)
+            { tempfloat = Vector3.Distance(go.transform.position, selectedLocObj.transform.position); selectedInt = tempint; }
+            tempint++;
+         }
+        dialSetting = selectedInt;
     }
 }
--- 442Unity/Assets/_scripts/RoomBuilder.cs
+++ 442Unity/Assets/_scripts/RoomBuilder.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RoomBuilder : MonoBehaviour
 {
     public GameObject itemSelectorDial,debugPrefab, debugPrefabVisual,activeVisualIndicator;
-    public List<GameObject> spawnableObjects;
+    public List<GameObject> spawnableObjects, tempSpawnableObjects;
+    public int currentItemSelected;
     public bool canSpawn;
     public RoomManager roomManager;
     // Start is called before the first frame update
@@ -17,17 +18,27 @@ public class RoomBuilder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Local mod:  " + Mathf.FloorToInt( itemSelectorDial.transform.localEulerAngles.x) / (360 / spawnableObjects.Count));
-        if (Input.GetAxis("Horizontal") != 0 )
+      
+       // currentItemSelected = Mathf.FloorToInt(itemSelectorDial.transform.localEulerAngles.x / (360 / spawnableObjects.Count));
+        Debug.Log( ":  Local Rot" + itemSelectorDial.transform.localEulerAngles.x + " : " + itemSelectorDial.transform.rotation.x + " : " + itemSelectorDial.transform.localRotation.x + " : " + currentItemSelected);
+        //Debug.Log("Local mod:  " + Mathf.FloorToInt( itemSelectorDial.transform.localEulerAngles.x) / (360 / spawnableObjects.Count));
+        if (Input.GetAxis("Vertical") != 0 )
         {
             RayForward();
+            if (activeVisualIndicator != null)
+            {
+                activeVisualIndicator.transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+            }
+
+
+                if (Input.GetKeyDown(KeyCode.JoystickButton5) || Input.GetKeyDown(KeyCode.JoystickButton4)) { Destroy(activeVisualIndicator); }// right and left grip buttons
         }
         
-        if (Input.GetAxis("Horizontal") == 0)
+        if (Input.GetAxis("Vertical") == 0)
         {
             if (activeVisualIndicator != null)
             {
-                GameObject clone = Instantiate(debugPrefab, activeVisualIndicator.transform.position, activeVisualIndicator.transform.rotation);
+                GameObject clone = Instantiate(spawnableObjects[currentItemSelected], activeVisualIndicator.transform.position, activeVisualIndicator.transform.rotation);
                 clone.transform.parent = roomManager.activeEnviroment.playerSpawnedObjects;
                 //clone.transform.LookAt(new Vector3(transform.position.x, clone.transform.position.y, transform.position.z));
                 Destroy(activeVisualIndicator);
@@ -39,9 +50,12 @@ public class RoomBuilder : MonoBehaviour
     }
     public void RayForward()
     {
-        int tempint = Mathf.FloorToInt(  (360 / spawnableObjects.Count) % itemSelectorDial.transform.localEulerAngles.x);
-        Debug.Log("Local Rot" + tempint + " : " + tempint);
-        Mathf.FloorToInt(itemSelectorDial.transform.localEulerAngles.x / spawnableObjects.Count);
+        //Calculate which object to spawn based on the rotation of the dial on the in game object
+        
+       
+
+       // Debug.Log("Local Rot" + currentItemSelected + " : " + currentItemSelected);
+      //  Mathf.FloorToInt(itemSelectorDial.transform.localEulerAngles.x / spawnableObjects.Count);
         RaycastHit objectHit;
 
         if (Physics.Raycast(transform.position, transform.forward, out objectHit, 50))
@@ -52,14 +66,19 @@ public class RoomBuilder : MonoBehaviour
                 {
                     //activeVisualIndicator = Instantiate(debugPrefabVisual, objectHit.point, objectHit.transform.rotation);
                     activeVisualIndicator.transform.position = objectHit.point;
-                    activeVisualIndicator.transform.LookAt(new Vector3(transform.position.x, activeVisualIndicator.transform.position.y, transform.position.z));
+                   // activeVisualIndicator.transform.LookAt(new Vector3(transform.position.x, activeVisualIndicator.transform.position.y, transform.position.z));
 
                 }
             }
             else
             {
+               // currentItemSelected = Mathf.FloorToInt(itemSelectorDial.transform.localEulerAngles.x / (350 / spawnableObjects.Count ));
+                currentItemSelected =  itemSelectorDial.GetComponent<Lever>().dialSetting;
+                //currentItemSelected--;
+                if (currentItemSelected >= spawnableObjects.Count) { currentItemSelected = spawnableObjects.Count - 1; }
+                if (currentItemSelected < 0) { currentItemSelected = 0; }
                 canSpawn = false;
-                activeVisualIndicator = Instantiate(debugPrefabVisual, objectHit.point, objectHit.transform.rotation);
+                activeVisualIndicator = Instantiate(tempSpawnableObjects[currentItemSelected], objectHit.point, objectHit.transform.rotation);
                 activeVisualIndicator.transform.LookAt(new Vector3(transform.position.x, activeVisualIndicator.transform.position.y, transform.position.z));
                 // GameObject clone =  Instantiate(debugPrefab, objectHit.point, objectHit.transform.rotation);
                 // clone.transform.LookAt(new Vector3(transform.position.x,clone.transform.position.y, transform.position.z));
